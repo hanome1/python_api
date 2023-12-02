a1f7c98812aeefeb38543d36f093beaf973f20da
nick <divanchmo@gmail.com>
2023-12-02 17:49:34 +0300

    sem2

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 import pytest
 import requests
 import yaml
+from module import Site
 
 with open('config.yaml') as f:
     cfg = yaml.safe_load(f)
@@ -20,3 +21,28 @@ def token():
 @pytest.fixture()
 def post_create():
     res = requests.post(url=cfg['posts'], headers={'X-Auth-Token': token}, params={'title': 'Test title', 'description': cfg['post_test_desc'], 'content': 'test text content\n'*10})
+
+
+# seminar2
+
+@pytest.fixture()
+def login_field():
+    return """//*[@id="login"]/div[1]/label/input"""
+
+@pytest.fixture()
+def pas_field():
+    return """//*[@id="login"]/div[2]/label/input"""
+
+@pytest.fixture()
+def btn():
+    return """//*[@id="login"]/div[3]/button"""
+
+@pytest.fixture()
+def err_label():
+    return """//*[@id="app"]/main/div/div/div[2]/h2"""
+
+@pytest.fixture()
+def site():
+    site = Site(cfg["address"])
+    yield site
+    site.close()
